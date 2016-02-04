19ec5288711293509f8f5dafd09677a5149d6cb3
VinaySingh035 <csvinay.d@gmail.com>
2016-02-04 19:44:04 +0530

    creditsUpdated

--- javascript/links.js
+++ javascript/links.js
@@ -14,14 +14,3 @@ function android()
 	document.getElementById('x4img').style.display="none";
 	}
 }
-// function contributors()
-// {
-// 	if(document.getElementById('x4').innerHTML.indexOf('i')!=1)
-// 	{
-// 		window.location="https://github.com/fossasia/flappy-svg/blob/master/Contributors.md";
-// 	}
-// 	else
-// 	{
-	
-// 	}
-// }
