d81d98757619abe955e1bbdbb76e163450962d3d
VinaySingh035 <csvinay.d@gmail.com>
2016-02-06 14:18:19 +0530

    functionNameUpdated

--- javascript/links.js
+++ javascript/links.js
@@ -6,15 +6,15 @@ function android()
 	}
 	else
 	{
-	document.getElementById('x2').style.display="none";
-	document.getElementById('x1').style.display="none";
-	document.getElementById('x3').innerHTML="<div style='margin-top:20px;margin-left:-3px;'><b>Play Store</b></div>";
-	document.getElementById('x4').innerHTML="<div style='margin-top:15px;'><b>APK</b></div>";
-	document.getElementById('x3img').style.display="none";
-	document.getElementById('x4img').style.display="none";
+		document.getElementById('x2').style.display="none";
+		document.getElementById('x1').style.display="none";
+		document.getElementById('x3').innerHTML="<div style='margin-top:20px;margin-left:-3px;'><b>Play Store</b></div>";
+		document.getElementById('x4').innerHTML="<div style='margin-top:15px;'><b>APK</b></div>";
+		document.getElementById('x3img').style.display="none";
+		document.getElementById('x4img').style.display="none";
 	}
 }
-function playstore()
+function contributors()
 {
 	if(document.getElementById('x4').innerHTML.indexOf('i')!=1)
 	{
