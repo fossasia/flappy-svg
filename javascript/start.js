aa27b60a3a3f6a96fdccdd1f7d185e4bdfdb4f2a
mukesh <mukesh14149@iiitd.ac.in>
2016-02-07 13:05:05 +0530

    Resolved issue #60

--- javascript/start.js
+++ javascript/start.js
@@ -5,7 +5,35 @@ window.onload = function() {
     scaleToFullscreen();
     showStartScreen();
 
+
+
     //Default Layer
-    desertShow();
-    flappy = new Flappy("bird");
+    var MAX = 2;
+	var rand = Math.floor((Math.random() * MAX) + 1);
+	if (rand == 1) {
+    		backgroundChange('background')
+	}
+	if (rand == 2) {
+    		backgroundChange('Gotham')
+	}
+
+	var MAX = 5;
+	var rand = Math.floor((Math.random() * MAX) + 1);
+	if (rand == 1) {
+    		characterChange('bird')
+	}
+	if (rand == 2) {
+    		characterChange('bat')
+	}
+	
+	if (rand == 3) {
+    		characterChange('alien')
+	}
+	if (rand == 4) {
+    		characterChange('flappydino')
+	}
+	if (rand == 5) {
+    		characterChange('helicopter')
+	}
+	
 }
