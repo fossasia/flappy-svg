e56c5a1cc8e6f9c1995da031b678185f2f45e6d6
VinaySingh035 <csvinay.d@gmail.com>
2016-02-09 16:55:58 +0530

    links in links.html fixed

--- javascript/links.js
+++ javascript/links.js
@@ -1,29 +1,29 @@
-function android()
+function android(instance)
 {
-	if(document.getElementById('x3').innerHTML.indexOf('i')!=1)
+	if(instance==1)
+	{
+		document.getElementById('x2').style.display="none";
+		document.getElementById('x1').style.display="none";
+		document.getElementById('x3').innerHTML="<div style='margin-top:20px;margin-left:-3px;'><a href='javascript:android(0)'><b>Play Store</b></a></div>";
+		document.getElementById('x4').innerHTML="<div style='margin-top:15px;'><a href='javascript:contributors(0)'><b>APK</b></a></div>";
+		document.getElementById('x3img').style.display="none";
+		document.getElementById('x4img').style.display="none";
+	}
+	else
 	{
 		
 		window.open('https://play.google.com/store/apps/details?id=fossasia.flappysvg','_blank');
         	
 	}
-	else
-	{
-		document.getElementById('x2').style.display="none";
-		document.getElementById('x1').style.display="none";
-		document.getElementById('x3').innerHTML="<div style='margin-top:20px;margin-left:-3px;'><b>Play Store</b></div>";
-		document.getElementById('x4').innerHTML="<div style='margin-top:15px;'><b>APK</b></div>";
-		document.getElementById('x3img').style.display="none";
-		document.getElementById('x4img').style.display="none";
-	}
 }
-function contributors()
+function contributors(instance)
 {
-	if(document.getElementById('x4').innerHTML.indexOf('i')!=1)
-	{
-		window.location="https://github.com/fossasia/flappy-svg/blob/master/FlappySVG_Android/app-release.apk?raw=true";
-	}
-	else
+	if(instance==1)
 	{
 		window.location="credits.html";
+	}
+	else
+	{
+		window.location="https://github.com/fossasia/flappy-svg/blob/master/FlappySVG_Android/app-release.apk?raw=true";
 	}
 }
