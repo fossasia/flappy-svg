309b302c973a6ec69ec1e23e1b22c7ea4b36a40b
kamishetty sreeja <sreeja25kamishetty@gmail.com>
2016-02-18 05:07:56 +0530

    added main menu

---
+++ javascript/main_menu.js
@@ -0,0 +1,31 @@
+function mainmenu(instance)
+{
+	if(instance==1)
+	{
+		window.location="index.html";
+	}
+	else if(instance==2)
+	{
+		window.location="howtoplay.html";
+		
+	}
+
+	else if(instance==3)
+	{
+		window.location="credits.html";
+		
+	}
+
+	else if(instance==4)
+	{
+	window.location="leaderboard.html";
+			
+	}
+	
+	else if(instance==5)
+	{
+	window.location="links.html";
+			
+	}
+	
+}
