2e2f7acf2e159a4e787e968d5ed673995d1c174e
Ridhwanul Haque <rhemon19@gmail.com>
2016-12-12 09:38:35 +0600

    Added Character "Botty"

--- javascript/start.js
+++ javascript/start.js
@@ -7,7 +7,7 @@ window.onload = function() {
     scaleToFullscreen();
     showStartScreen();
 
-    var characters = ["bird", "bat", "alien","flappydino","helicopter", "rocket","Santa", "Super_rocket", "Flappyfish"];
+    var characters = ["bird", "bat", "alien","flappydino","helicopter", "rocket","Santa", "Super_rocket", "Flappyfish", "botty"];
     var backgrounds=["background","Gotham","space","Snow", "Sea"];
 
     rand=Math.floor((Math.random() * (backgrounds.length)));
