72fa582fba7a4277a5254c0592837b603b7b4930
Ridhwanul Haque <rhemon19@gmail.com>
2016-12-13 18:40:51 +0600

    Replacing .app with .dmg (#178)

--- FlappySVG_Mac/FlappySVG.app/Contents/MacOS/data/content/javascript/score.js
+++
@@ -1,5 +0,0 @@
-function updateScore(){
-	var score_element = document.getElementById("score");
-	var score = Number(score_element.innerHTML);
-	score_element.innerHTML = score+1;
-}
--- FlappySVG_Mac/FlappySVG.app/Contents/Resources/script
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-basedir=`dirname "$0"`
-$basedir/data/bin/node --harmony $basedir/data/app.js & wait
