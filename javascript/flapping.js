4035696bfaa76e47b0a0ae500b979294dd926604
kzeer <kzeer@umich.edu>
2021-12-08 12:54:06 -0500

    distance counter consistent for banner and popup

--- javascript/flapping.js
+++ javascript/flapping.js
@@ -6,7 +6,6 @@ function Flappy(layerName) {
     var me = this;
     this.action = new Action(function(){me.fly()}, function(){me.started()});
     document.onmousedown = function () { me.flap() };
-    this.myDist = 0;
 }
 
 Flappy.prototype = {
@@ -54,7 +53,6 @@ Flappy.prototype = {
 
     checkCollision : function() {
         updateDistance();
-        this.myDist = this.myDist + 1;
         var flappy_rect = this.layer.getBoundingClientRect();
         for (i = 0; i < 3; i++) {
             if (i >= obstacles.length)
@@ -71,7 +69,7 @@ Flappy.prototype = {
             } else {
 				var gameOverSound = new Audio("../flappy-svg/Sounds/GameOver.mp3");
 				gameOverSound.play();
-				alert('Game Over :( Final Score: ' + Number(document.getElementById("tspan17169").innerHTML) + ', Distance: ' + this.myDist);
+				alert('Game Over :( Final Score: ' + Number(document.getElementById("tspan17169").innerHTML) + ', Distance: ' + Number(document.getElementById("tspan_distance").innerHTML));
 			}
 
             if (c || o_rect.right < flappy_rect.left)
